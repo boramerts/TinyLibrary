4cb7cd9614ede52f615c3a8e4925ab6d95ad26ed
boramerts <mertbora723@gmail.com>
2023-12-08 23:27:48 +0300

    Initial Commit

---
+++ TinyLibrary/TinyLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyLibraryApp.swift
+//  TinyLibrary
+//
+//  Created by Bora Mert on 8.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TinyLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
